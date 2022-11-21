21e77ea94f48608031fa7da47c6218929cccd649
Rotem10 <65648972+Rotem10@users.noreply.github.com>
2022-11-21 16:11:39 +0200

    convert to typeScript

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function info() {
+    alert("Rotem Yosef \n version 1.0 \n Calculator");
+}
+function displayButtonInfo(v) {
+    alert(`${v}`);
+}
+function changeBgc(v) {
+    const body = document.querySelector("body");
+    body.style.backgroundColor = v;
+}
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+function info() {
+    alert("Rotem Yosef \n version 1.0 \n Calculator");
+  }
+  
+  function displayButtonInfo(v: string) {
+    alert(`${v}`);
+  }
+  
+  function changeBgc(v: string) {
+  const body: HTMLElement = document.querySelector("body") as HTMLElement
+  body.style.backgroundColor = v
+  }
+  
